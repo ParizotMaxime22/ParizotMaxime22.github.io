5ee90d09f5f664897a3d11ea7bca54a3dd3730ef
Maxime Parizot <parizot.maxime22@gmail.com>
2025-04-22 14:11:44 +0200

    Correction affichage des images de projets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2976701968"
+    serviceWorkerVersion: "4144962042"
   }
 });
